e38b818960f65db8109541bd534bebc1145122c2
“Shaun <shaunwmusic@gmail.com>
2016-10-27 17:11:33 -0500

    Add full-page div

--- app/index.js
+++ app/index.js
@@ -1,1 +1,34 @@
 import 'whatwg-fetch';
+import Vue from 'vue/dist/vue';
+
+const app = new Vue({
+  el: '.app',
+
+  data() {
+    return {
+      results: null,
+    };
+  },
+
+  mounted() {
+    fetch('http://gateway.marvel.com:80/v1/public/series/9856?apikey=d95eb5768c013b90d3d198d41e5c0502')
+        .then((r) => r.json())
+        .then((data) => {
+          this.title = data.results;
+
+          this.choosePokemon(data.results);
+        });
+  },
+
+  methods: {
+    choosePokemon(pokemon) {
+      this.loading = true;
+      fetch(pokemon.url)
+      .then((r) => r.json())
+      .then((data) => {
+        this.loading = false;
+        this.currentSelection = data;
+      });
+    },
+  },
+});
